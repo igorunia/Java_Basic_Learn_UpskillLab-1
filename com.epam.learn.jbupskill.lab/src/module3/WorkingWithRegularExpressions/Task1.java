e8eca95530b490e1c3cf550b614dd133f329dfbc
igorunia <igor.sevostyanchik@mail.ru>
2021-01-25 23:56:12 +0300

    Module 3 StringBuilder are completed

--- com.epam.learn.jbupskill.lab/src/module3/WorkingWithRegularExpressions/Task1.java
+++ com.epam.learn.jbupskill.lab/src/module3/WorkingWithRegularExpressions/Task1.java
@@ -2,72 +2,70 @@ package module3.WorkingWithRegularExpressions;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Cоздать приложение,разбирающее текст(текст хранится в строке)и позволяющее выполнять с текстом
- * три различных операции:отсортировать абзацы по количеству предложений;
+ * Cоздать приложение,разбирающее текст(текст хранится в строке)и позволяющее выполнять с текстом три различных операции:
+ * отсортировать абзацы по количеству предложений;
  * в каждом предложении отсортировать слова по длине;
- * отсортировать лексемы в предложении по убыванию количества вхождений заданного символа,
- * а в случае равенства – по алфавиту.
+ * отсортировать лексемы в предложении по убыванию количества вхождений заданного символа,а в случае равенства – по алфавиту.
  */
 public class Task1 {
     public static void main(String[] args) {
-        String text = "befnknjnfjknekjn. jnfsjksnn. kfjejkjknkjsnjk.\n njsnsnjnsns.njwnsjkns.\n dsjanfjksanfjfkgjsjs. " +
-                "ahfgahgfhaghah.jkafajjhffajhjfah.ajafhahjafhahjzfkahfjah.\n";
+        String text = "befnknj. jnfsjksnnamafmna. kfjejkjknkj.\n njsnsnjnsns.\n dsjanfjksanfjfkgjsjs. " +
+                "ahfgahgfhaghah. jkafajjhffajhjfah. ajafhahjafhahjzfkahfjah.\n";
 
-        System.out.println("Choose one of the sorting method: 1 - method1, 2 - method2, 3 - method3");
+        System.out.println("Choose one of the sorting method:\n" +
+                " 1 - отсортировать абзацы по количеству предложений;\n" +
+                " 2 - в каждом предложении отсортировать слова по длинне;\n" +
+                " 3 - отсортировать лексемы в предложении по убыванию количества вхождений заданного символа,а в случае равенства – по алфавиту.");
         Scanner scanner = new Scanner(System.in);
         int method = scanner.nextInt();
         if (method == 1) {
-            ArrayList<String> listText = new ArrayList<>();
-            Pattern pattern = Pattern.compile("[^\\\\n]+");
+            List<String> listText = new ArrayList<>();
+            Pattern pattern = Pattern.compile("[^\\n]+");
             Matcher matcher = pattern.matcher(text);
 
             while (matcher.find()) {
                 String group = matcher.group();
                 listText.add(group);
+            }
 
-                listText.sort(new Comparator<String>() {
-                    @Override
-                    public int compare(String a, String b) {
-                        return Integer.compare(sentenceCount(a), sentenceCount(b));
-                    }
-                });
+            listText.sort(Comparator.comparingInt(Task1::sentenceCount));
+            for (String s : listText) {
 
-                System.out.print(group);
+                System.out.println(s);
             }
         }
 
 
         if (method == 2) {
-            ArrayList<String> listText = new ArrayList<>();
-            Pattern pattern = Pattern.compile("[^.]+");
+            List<String> listText = new ArrayList<>();
+            Pattern pattern = Pattern.compile("[^.!?]+");
             Matcher matcher = pattern.matcher(text);
 
             while (matcher.find()) {
-                String group = matcher.group();
-                listText.add(group);
-
-                listText.sort(new Comparator<String>() {
-                    @Override
-                    public int compare(String a, String b) {
-                        return Integer.compare(sentenceWordCount(a), sentenceWordCount(b));
-                    }
-                });
-
-                System.out.print(group);
+                String group2 = matcher.group();
+                listText.add(group2);
             }
-        }
 
+            listText.sort((a, b) -> Integer.compare(sentenceWordCount(a), sentenceWordCount(b)));
+
+            for (String word : listText) {
+
+                System.out.println(word);
+            }
+
+        }
 
     }
 
     private static int sentenceCount(String group) {
-        ArrayList<String> listText = new ArrayList<>();
-        Pattern pattern = Pattern.compile("[^.]+");
+        List<String> listText = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[^.!?]+");
         Matcher matcher = pattern.matcher(group);
         while (matcher.find()) {
             String group2 = matcher.group();
@@ -78,7 +76,7 @@ public class Task1 {
     }
 
     private static int sentenceWordCount(String group) {
-        ArrayList<String> listText = new ArrayList<>();
+        List<String> listText = new ArrayList<>();
         Pattern pattern = Pattern.compile("[^\\s]+");
         Matcher matcher = pattern.matcher(group);
         while (matcher.find()) {
