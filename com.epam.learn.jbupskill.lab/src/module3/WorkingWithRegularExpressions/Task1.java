31ebb68a329020d30396b35f6b5751d2f9f09202
igorunia <igor.sevostyanchik@mail.ru>
2021-01-27 16:25:53 +0300

    Module 3 StringBuilder are completed

--- com.epam.learn.jbupskill.lab/src/module3/WorkingWithRegularExpressions/Task1.java
+++ com.epam.learn.jbupskill.lab/src/module3/WorkingWithRegularExpressions/Task1.java
@@ -15,12 +15,14 @@ import java.util.regex.Pattern;
  */
 public class Task1 {
     public static void main(String[] args) {
-        String text = "befnknj. jnfsjksnnamafmna. kfjejkjknkj.\n njsnsnjnsns.\n dsjanfjksanfjfkgjsjs. " +
-                "ahfgahgfhaghah. jkafajjhffajhjfah. ajafhahjafhahjzfkahfjah.\n";
+        String text = "aa. aaaaa. a!\n" +
+                "Второй абзац. Пять предложений. Это третье предложение. Это четвертое. И это пятое...\n" +
+                "Третий абзац и всего одно предложение.\n" +
+                "Четвертый абзац. Привет, приветы, приветули.";
 
         System.out.println("Choose one of the sorting method:\n" +
                 " 1 - отсортировать абзацы по количеству предложений;\n" +
-                " 2 - в каждом предложении отсортировать слова по длинне;\n" +
+                " 2 - в каждом предложении отсортировать слова по длине;\n" +
                 " 3 - отсортировать лексемы в предложении по убыванию количества вхождений заданного символа,а в случае равенства – по алфавиту.");
         Scanner scanner = new Scanner(System.in);
         int method = scanner.nextInt();
@@ -28,20 +30,15 @@ public class Task1 {
             List<String> listText = new ArrayList<>();
             Pattern pattern = Pattern.compile("[^\\n]+");
             Matcher matcher = pattern.matcher(text);
-
             while (matcher.find()) {
                 String group = matcher.group();
                 listText.add(group);
             }
-
             listText.sort(Comparator.comparingInt(Task1::sentenceCount));
             for (String s : listText) {
-
                 System.out.println(s);
             }
         }
-
-
         if (method == 2) {
             List<String> listText = new ArrayList<>();
             Pattern pattern = Pattern.compile("[^.!?]+");
@@ -51,14 +48,48 @@ public class Task1 {
                 String group2 = matcher.group();
                 listText.add(group2);
             }
-
-            listText.sort((a, b) -> Integer.compare(sentenceWordCount(a), sentenceWordCount(b)));
-
             for (String word : listText) {
+                List<String> listWords = new ArrayList<>();
+                Pattern pattern2 = Pattern.compile("[^\\s]+");
+                Matcher matcher2 = pattern2.matcher(word);
+                while (matcher2.find()) {
+                    String group2 = matcher2.group();
+                    listWords.add(group2);
+                }
+                listWords.sort((a,b)-> Integer.compare(a.length(),b.length()));
 
-                System.out.println(word);
+                for(String word2 : listWords) {
+
+                    System.out.print(word2 + " ");
+                }
+                System.out.println(".");
             }
+        }
+        if (method == 3) {
+            List<String> listText = new ArrayList<>();
+            Pattern pattern = Pattern.compile("[^.!?]+");
+            Matcher matcher = pattern.matcher(text);
 
+            while (matcher.find()) {
+                String group3 = matcher.group();
+                listText.add(group3);
+            }
+            for (String word3 : listText) {
+                List<String> listWords3 = new ArrayList<>();
+                Pattern pattern3 = Pattern.compile("(\\w)\\1+");
+                Matcher matcher3 = pattern3.matcher(word3);
+                while (matcher3.find()) {
+                    String group3 = matcher3.group();
+                    listWords3.add(group3);
+                }
+                listWords3.sort(Comparator.comparingInt(Task1::lexmeCount));
+
+                for(String word4 : listWords3) {
+
+                    System.out.print(word4 + " ");
+                }
+                System.out.println(".");
+            }
         }
 
     }
@@ -72,17 +103,16 @@ public class Task1 {
             listText.add(group2);
         }
         return listText.size();
-
     }
-
-    private static int sentenceWordCount(String group) {
+    private static int lexmeCount(String group) {
         List<String> listText = new ArrayList<>();
-        Pattern pattern = Pattern.compile("[^\\s]+");
+        Pattern pattern = Pattern.compile("(\\w)\\1+");
         Matcher matcher = pattern.matcher(group);
         while (matcher.find()) {
-            String group3 = matcher.group();
-            listText.add(group3);
+            String group2 = matcher.group();
+            listText.add(group2);
         }
         return listText.size();
     }
+
 }
