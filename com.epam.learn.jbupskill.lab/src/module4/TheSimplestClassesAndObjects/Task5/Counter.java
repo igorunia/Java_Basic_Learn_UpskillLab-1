e7ccee5472da11eb4050706d4bb208dcaf791d87
igorunia <igor.sevostyanchik@mail.ru>
2021-02-22 20:57:11 +0300

    Module 4 Task1-5 completed))

---
+++ com.epam.learn.jbupskill.lab/src/module4/TheSimplestClassesAndObjects/Task5/Counter.java
@@ -0,0 +1,58 @@
+package module4.TheSimplestClassesAndObjects.Task5;
+
+/**
+ * Опишите класс, реализующий десятичный счетчик, который может увеличивать или уменьшать свое значение на единицу
+ * в заданном диапазоне. Предусмотрите инициализацию счетчика значениями по умолчанию и произвольными значениями.
+ * Счетчик имеет методы увеличения и уменьшения состояния, и метод позволяющее получить его текущее состояние.
+ * Написать код, демонстрирующий все возможности класса.
+ */
+
+public class Counter {
+    private int currentValue;
+    private int minValue;
+    private int maxValue;
+
+    Counter() {
+        this.currentValue = 0;
+        this.minValue = 0;
+        this.maxValue = 10;
+    }
+
+    public Counter(int currentValue, int minValue, int maxValue) {
+        this.currentValue = currentValue;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    public int getMinValue() {
+        return minValue;
+    }
+
+    public int getMaxValue() {
+        return maxValue;
+    }
+
+    public void increaseValue() {
+        currentValue++;
+        if (currentValue == maxValue) {
+            System.out.println("Upper Range Reached");
+        } else if (currentValue > maxValue) {
+            System.out.println("Invalid overshoot of the range");
+        }
+        currentValue--;
+    }
+
+    public void reduceValue() {
+        currentValue--;
+        if (currentValue == minValue) {
+            System.out.println("Upper Range Reached");
+        } else if (currentValue < minValue) {
+            System.out.println("Invalid overshoot of the range");
+        }
+        currentValue++;
+    }
+}
---
+++ com.epam.learn.jbupskill.lab/src/module4/TheSimplestClassesAndObjects/Task5/Runner.java
@@ -0,0 +1,13 @@
+package module4.TheSimplestClassesAndObjects.Task5;
+
+public class Runner {
+    public static void main(String args[]){
+
+
+
+
+
+
+    }
+
+}
