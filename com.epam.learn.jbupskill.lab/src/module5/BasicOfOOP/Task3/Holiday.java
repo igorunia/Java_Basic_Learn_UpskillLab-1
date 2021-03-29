bea85ad3cdce6c708882cb5da16c7691262fba71
igorunia <igor.sevostyanchik@mail.ru>
2021-03-29 23:44:46 +0300

    Module 5 Task 3

---
+++ com.epam.learn.jbupskill.lab/src/module5/BasicOfOOP/Task3/Holiday.java
@@ -0,0 +1,29 @@
+package module5.BasicOfOOP.Task3;
+
+import java.time.LocalDate;
+
+public class Holiday {
+    private LocalDate holidayDate;
+    private String nameOfHoliday;
+
+    public Holiday(LocalDate holidayDate, String nameOfHoliday) {
+        this.holidayDate = holidayDate;
+        this.nameOfHoliday = nameOfHoliday;
+    }
+
+    public LocalDate getHolidayDate() {
+        return holidayDate;
+    }
+
+    public void setHolidayDate(LocalDate holidayDate) {
+        this.holidayDate = holidayDate;
+    }
+
+    public String getNameOfHoliday() {
+        return nameOfHoliday;
+    }
+
+    public void setNameOfHoliday(String nameOfHoliday) {
+        this.nameOfHoliday = nameOfHoliday;
+    }
+}
---
+++ com.epam.learn.jbupskill.lab/src/module5/BasicOfOOP/Task3/Runner.java
@@ -0,0 +1,23 @@
+package module5.BasicOfOOP.Task3;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Создать класс Календарь с внутренним классом,
+ * с помощью объектов которого можно хранить информацию о выходных и праздничных днях
+ */
+public class Runner {
+    public static void main(String args[]){
+        int year = 2021;
+        Calendar calendar = new Calendar(year);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        calendar.addHoliday(LocalDate.parse("01.01." + year, dtf), "New Year");
+
+
+        calendar.printHoliday();
+
+    }
+
+}
